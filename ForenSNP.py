919c0165892a32bdfbdda65fab58363de78254d1
renzilin <zilin.ren@outlook.com>
2020-09-02 17:39:11 +0800

    v0.1

---
+++ ForenSNP.py
@@ -0,0 +1,100 @@
+
+"""
+
+
+    - ForenRepeat ver0.1
+    - Written by Ren Zilin; zilin.ren@outlook.com
+    - For reads from the forenseq kit and nanopore platform 
+
+
+"""
+
+import os
+import sys
+import pathlib
+import argparse
+
+## local pack
+from scripts import myGenotypeCaller
+
+def run_mpileup(samtools_path, ref_path, snp_bed_path, bam_file_path, output_pileup):
+    cmd = "%s mpileup -f %s -l %s %s -o %s" % (samtools_path, ref_path, snp_bed_path, bam_file_path, output_pileup)
+    sys.stdout.write('## Running command: `%s`\n' % cmd)
+    os.system(cmd)
+    return
+
+def run_isnv(perl_path, output_pileup, output_ntfreq):
+    cmd = "%s ./scripts/myISNV.pl -in %s -out %s" % (perl_path, output_pileup, output_ntfreq)
+    sys.stdout.write('## Running command: `%s`\n' % cmd)
+    os.system(cmd)
+    return
+
+def getgeno(args):
+    ## tools' path
+    samtools_path = args.SAMTOOLS
+    perl_path     = args.PERL
+    
+    ## files' path
+    ref_path      = args.ref
+    snp_bed_path  = args.snp
+    bam_file_path = args.bam
+    
+    ## parameter set
+    valid_cutoff  = args.cutoff
+    
+    ## input
+    sampleID   = args.id
+    
+    ## out dir
+    output_dir = './output/%s' % sampleID
+    pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
+    
+    ## out files
+    output_pileup = '%s/%s.mpileup' % (output_dir, sampleID)
+    output_ntfreq = '%s/%s.ntfreq'  % (output_dir, sampleID)
+    output_genotype = '%s/%s.csv'   % (output_dir, sampleID)
+    
+    ## work flow
+    run_mpileup(samtools_path, ref_path, snp_bed_path, bam_file_path, output_pileup)
+    run_isnv(perl_path, output_pileup, output_ntfreq)
+    myGenotypeCaller.get_alleles(output_ntfreq, snp_bed_path, output_genotype, valid_cutoff)
+    
+    return
+
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='SNP genotype caller for data from forenseq kit.', 
+                                     usage='''python ForenSNP.py <command> [<args>]
+
+Available commands are:    
+    getgeno    simple genotype caller based on iSNV
+    ''')
+    subparsers = parser.add_subparsers(help='sub-command help')
+    subparsers.required = True
+
+    ############## getgeno
+    getgeno_parser = subparsers.add_parser('getgeno', help='simple genotype caller based on iSNV')
+    
+    ## required arguments
+    getgeno_parser.add_argument('--ref', required = True, help='path to genome reference', default = None)
+    getgeno_parser.add_argument('--snp', required = True, help='path to the snp information file, see the USAGE for format', default = None)
+    getgeno_parser.add_argument('--bam', required = True, help='path to the bam file', default = None)
+    getgeno_parser.add_argument('--id', required = True, help='sample name for output file', default = 'sample')
+    
+    
+    
+    ## optional
+    getgeno_parser.add_argument('--SAMTOOLS', required = False, help='path to samtools', default = 'samtools')
+    getgeno_parser.add_argument('--PERL', required = False, help='path to perl', default = 'perl')
+    getgeno_parser.add_argument('--cutoff', required = False, 
+                                help='the cutoff for validation of genotypes; recommand range 0.1-0.15 for nanopore data', default = 0.1)
+ 
+    getgeno_parser.set_defaults(func=getgeno)
+
+
+    ## run
+    args = parser.parse_args()
+    args.func(args)
---
+++ scripts/myGenotypeCaller.py
@@ -0,0 +1,82 @@
+
+"""
+
+## Create: Zilin Ren
+## Date:   2020-09-02
+## Email:  Zilin.Ren@outlook.com
+
+## Workflow:
+    1. output: genotype and coverage
+
+"""
+
+import numpy as np
+import pandas as pd 
+
+
+def func_cal_coverage(x):
+    coverage = sum([x["A"], x["G"], x["C"], x["T"]])
+    return coverage
+
+
+def func_annotate_rs_into_ntfreq(ntfreq_file, rs_ref_path):
+    
+    ## search for each line
+    def search_rs_num(x, rs_ref):
+        rs_arr = rs_ref.loc[(rs_ref['#chr'] == x['#chr']) 
+                            & (rs_ref['start'] <= int(x['pos']))
+                            & (rs_ref['end'] >= int(x['pos'])), 'rs'].values
+        return rs_arr[0]
+    
+    ## 
+    ntfreq_dat = pd.read_csv(ntfreq_file, sep="\t")
+    rs_ref = pd.read_csv(rs_ref_path, sep="\t", header=None)
+    rs_ref.columns = ['#chr', 'start', 'end', 'rs', 'allele']
+    ntfreq_dat['rs'] = ntfreq_dat.apply(lambda x: search_rs_num(x, rs_ref), axis=1)
+    ntfreq_dat['ref'] = ntfreq_dat['ref'].apply(lambda x: x.upper())
+    return ntfreq_dat
+
+
+def get_allele_each_row(x, valid_cutoff):
+    coverage= x['cov']
+    ratio_A = float(x['A']) / coverage
+    ratio_G = float(x['G']) / coverage
+    ratio_C = float(x['C']) / coverage
+    ratio_T = float(x['T']) / coverage
+
+    freq_df = pd.DataFrame([['A', ratio_A], ['G', ratio_G], ['C', ratio_C], ['T', ratio_T]], columns=['allele', 'ratio'])
+    freq_df.sort_values(by = 'ratio', ascending = False, inplace=True)
+    INFO    = "".join(["{}:{:.2%};".format(i[0], i[1]) for i in freq_df.values.tolist()])
+
+    if freq_df.loc[freq_df['ratio'] > valid_cutoff, :].shape[0] == 1:
+        allele1 = freq_df['allele'].values[0]
+        allele2 = allele1
+
+    elif freq_df.loc[freq_df['ratio'] > valid_cutoff, :].shape[0] == 2:
+        allele1 = freq_df['allele'].values[0]
+        allele2 = freq_df['allele'].values[1]
+
+    else:
+        allele1 = '?' + freq_df['allele'].values[0]
+        allele2 = '?' + freq_df['allele'].values[1]
+
+    return [allele1, allele2, coverage, INFO]
+
+
+
+def get_alleles(output_ntfreq, snp_bed_path, output_genotype, valid_cutoff):
+
+
+
+    
+    ntfreq_df = func_annotate_rs_into_ntfreq(output_ntfreq, snp_bed_path)
+    ntfreq_df['cov'] = ntfreq_df.apply(lambda x: func_cal_coverage(x), axis=1)
+    
+    
+    alleles_series = ntfreq_df.apply(lambda x: get_allele_each_row(x, valid_cutoff), axis=1)
+    alleles_df = pd.DataFrame(alleles_series.tolist(), columns=['allele1', 'allele2', 'totalCoverage', 'INFOs'])
+
+    raw_output_df = pd.concat([ntfreq_df, alleles_df], axis=1)
+    output_df = raw_output_df.loc[:, ['#chr', 'pos', 'rs', 'ref', 'allele1', 'allele2', 'totalCoverage', 'INFOs']]
+    output_df.to_csv(output_genotype, index=None)
+    return 
