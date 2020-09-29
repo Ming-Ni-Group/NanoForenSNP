d9cc71c42e381e33f185381c281eb71442f9138c
renzilin <zilin.ren@outlook.com>
2020-09-29 21:47:04 +0800

    updated forensnp

--- forensnp_scripts/myGenotypeCaller.py
+++ forensnp_scripts/myGenotypeCaller.py
@@ -42,20 +42,21 @@ def get_allele_each_row(x, homo_cutoff, cov_cutoff=10):
     coverage= x['cov']
 
     if coverage == 0:
-        return [pd.NA, pd.NA, coverage, 'no-coverage']
+        return [pd.NA, pd.NA, coverage, 'no-coverage'] + [0 for i in range(8)]
 
-    
+    forware_reverse = x["A5'/3'"].split('/') + x['G5/3'].split('/') + x['C5/3'].split('/') + x['T5/3'].split('/')
+
     ratio_A = float(x['A']) / coverage
     ratio_G = float(x['G']) / coverage
     ratio_C = float(x['C']) / coverage
     ratio_T = float(x['T']) / coverage
 
     freq_df = pd.DataFrame([['A', ratio_A], ['G', ratio_G], ['C', ratio_C], ['T', ratio_T]], columns=['allele', 'ratio'])
-    freq_df.sort_values(by = 'ratio', ascending = False, inplace=True)
     INFO    = "".join(["{}:{:.2%};".format(i[0], i[1]) for i in freq_df.values.tolist()])
+    freq_df.sort_values(by = 'ratio', ascending = False, inplace=True)
 
     if coverage < cov_cutoff:
-        return [pd.NA, pd.NA, coverage, INFO]
+        return [pd.NA, pd.NA, coverage, INFO] + forware_reverse
 
     if freq_df.loc[freq_df['ratio'] > homo_cutoff, :].shape[0] == 1:
         allele1 = freq_df['allele'].values[0]
@@ -69,7 +70,7 @@ def get_allele_each_row(x, homo_cutoff, cov_cutoff=10):
         allele1 = '?' + freq_df['allele'].values[0]
         allele2 = '?' + freq_df['allele'].values[1]
 
-    return [allele1, allele2, coverage, INFO]
+    return [allele1, allele2, coverage, INFO] + forware_reverse
 
 
 
@@ -83,10 +84,10 @@ def get_alleles(sampleID, output_ntfreq, snp_bed_path, output_genotype, homo_cut
     
     
     alleles_series = ntfreq_df.apply(lambda x: get_allele_each_row(x, homo_cutoff), axis=1)
-    alleles_df = pd.DataFrame(alleles_series.tolist(), columns=['allele1', 'allele2', 'totalCoverage', 'INFOs'])
+    alleles_df = pd.DataFrame(alleles_series.tolist(), columns=['allele1', 'allele2', 'totalCoverage', 'INFOs', 'A+', 'A-', 'G+', 'G-', 'C+', 'C-', 'T+', 'T-'])
 
     raw_output_df = pd.concat([ntfreq_df, alleles_df], axis=1)
-    output_df = raw_output_df.loc[:, ['#chr', 'pos', 'rs', 'ref', 'allele1', 'allele2', 'totalCoverage', 'INFOs']]
+    output_df = raw_output_df.loc[:, ['#chr', 'pos', 'rs', 'ref', 'allele1', 'allele2', 'totalCoverage', 'INFOs', 'A+', 'A-', 'G+', 'G-', 'C+', 'C-', 'T+', 'T-']]
     output_df['SampleID'] = sampleID
     output_df.to_csv(output_genotype, index=None, na_rep="NA")
     return 
