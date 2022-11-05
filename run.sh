0a953bea9a8447557e7237be4d540195b9d9dee4
renzilin <zilin.ren@outlook.com>
2022-11-05 15:01:23 +0800

    nanoforenstr2

--- run.sh
+++ run.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-fastq=$1
-oudir=$2
+fastq=$1   # dir of output of minknow (such as: barcodeXX, barcodeXX is a directory that stores multi-fastq files)
+oudir=$2   
 
 fullname=${fastq##*/}
 stem=${fullname%.*}
-bam=$stem.bam
 
 refer=/home/renz/Projects/nanopore/NanoForenSNP/config_file/ref_472.fa
 snpnb=/home/renz/Projects/nanopore/NanoForenSNP/config_file/ref_472.cfg
 
+mkdir -p $oudir/fastqs
 mkdir -p $oudir/alignment
-minimap2 -ax map-ont ${refer} ${fastq} | samtools view -S -b | samtools sort > $oudir/alignment/${bam}
-samtools index $oudir/alignment/${bam}
+mkdir -p $oudir/snp_out
 
-python /home/renz/Projects/nanopore/NanoForenSNP/NanoForenSNP.py getgeno --ref ${refer} --snp ${snpnb} --bam $oudir/alignment/${bam} --out $oudir/snp_out/${stem}
+cat ${fastq}/*.fastq > $oudir/fastqs/${stem}.fq
+
+minimap2 -ax map-ont ${refer} $oudir/fastqs/${stem}.fq | samtools view -S -b | samtools sort > $oudir/alignment/$stem.bam
+
+samtools index $oudir/alignment/$stem.bam
+
+python /home/renz/Projects/nanopore/NanoForenSNP/NanoForenSNP.py getgeno --ref ${refer} --snp ${snpnb} --bam $oudir/alignment/$stem.bam --out $oudir/snp_out/${stem}
